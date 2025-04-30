3e4747181a1bf0b1c0b115b015604ba8bc5f5a8b
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2025-04-30 11:44:44 +0200

    Fix login fixture

--- conftest.py
+++ conftest.py
@@ -1,15 +1,17 @@
 import pytest
 
+from config import settings
+
 
 @pytest.fixture(scope='session')
-def login(playwright):
+def login(playwright, base_url: settings.BASE_URL ,email: settings.EMAIL, password: settings.PASSWORD):
     browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page
-    page.open_page('https://dev.abra-market.com/')
+    page.open_page(base_url)
     page.click_login_button()
-    page.fill_email_field('logego6633@hikuhu.com')
-    page.fill_password_field('Password1!')
+    page.fill_email_field(email)
+    page.fill_password_field(password)
     #page.click_login_button_login_page()
     page.click_login_button()
     page.check_personal_block()
