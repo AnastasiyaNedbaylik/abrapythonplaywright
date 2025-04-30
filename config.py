92c704e384f4bb8913956aeeceeac25d92026e4d
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2025-04-30 11:46:09 +0200

    Fix Settings

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ DOTENV = str(pathlib.Path(__file__).resolve().parents[1] / ".env")
 
 class Settings(pydantic_settings.BaseSettings):
     BASE_URL_API: str = ""
+    BASE_URL: str = ""
     ENVIRONMENT: str = ""
     EMAIL: str = ""
     PASSWORD: str = ""
