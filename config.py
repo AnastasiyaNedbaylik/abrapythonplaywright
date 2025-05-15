f33b75cb546e812e4005a41f00e5449280da32fb
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2025-05-15 09:17:09 +0200

    Fix configs

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Settings(BaseSettings):
     EMAIL: str = ""
     PASSWORD: str = ""
 
-    POSTGRES_DB: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
+    POSTGRES_DB: str = ""
+    POSTGRES_USER: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_HOST: str = ""
+    POSTGRES_PORT: int = ""
 
     model_config = SettingsConfigDict(env_file=str(DOTENV))
 
